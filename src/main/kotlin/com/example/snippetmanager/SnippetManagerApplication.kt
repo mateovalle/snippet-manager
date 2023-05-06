143f8b6e70db2c97c33f2cac7940023f493da3e2
mateovalle <mateovalleduran@gmail.com>
2023-05-06 18:57:01 -0300

    initial commit

---
+++ src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.snippetmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetManagerApplication>(*args)
+}
