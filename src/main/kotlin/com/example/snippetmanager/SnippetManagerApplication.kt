6ef91b95dcba629b67d20d557eff6a5207f3fa8c
segun <segundo.marcaida@ing.austral.edu.ar>
2023-05-26 12:58:51 -0300

    Snippet object

--- src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
+++ src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
@@ -4,7 +4,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SnippetManagerApplication
+@RestController
+class SnippetManagerApplication{
+    @GetMapping("/snippets")
+    fun snippets():List<Snippet>{
+        return listOf(
+            Snippet(1, "Snippet 1",  LocalDate.now()),
+            Snippet(2, "Snippet 2",  LocalDate.now()),
+            Snippet(3, "Snippet 3",  LocalDate.now()),
+            Snippet(4, "Snippet 4",  LocalDate.now()),
+        );
+    }
+
+}
+
 
 fun main(args: Array<String>) {
     runApplication<SnippetManagerApplication>(*args)
