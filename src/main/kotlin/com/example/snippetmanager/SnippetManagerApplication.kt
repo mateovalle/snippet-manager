35affb0d5449d8aee241e51e4e4354c561deb2df
matiaslcoulougian <matias.coulougian@ing.austral.edu.ar>
2023-05-28 10:53:00 -0300

    add missed changes

--- src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
+++ src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
@@ -1,10 +1,12 @@
 package com.example.snippetmanager
 
+import Snippet
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 
 @SpringBootApplication
 @RestController
--- src/main/kotlin/com/example/snippetmanager/entity/Snippet.kt
+++ src/main/kotlin/com/example/snippetmanager/entity/Snippet.kt
@@ -1,4 +1,29 @@
-package com.example.snippetmanager.entity
+import java.time.LocalDate
 
-class Snippet {
+//esta clase la cree para ver como devolvia los parametros Spring, dudo que la usemos.
+
+class Snippet(private var id:Long, private var content:String, private var lastModified: LocalDate) {
+    fun getId(): Long {
+        return id
+    }
+
+    fun getContent():String{
+        return content
+    }
+
+    fun getLastModified():LocalDate{
+        return lastModified
+    }
+
+    fun setId(id:Long){
+        this.id = id
+    }
+
+    fun setContent(content:String){
+        this.content = content
+    }
+
+    fun setLastModified(lastModified: LocalDate){
+        this.lastModified = lastModified
+    }
 }
