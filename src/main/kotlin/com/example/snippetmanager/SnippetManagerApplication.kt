f324ae238949555f99094d14fafd541a1bd22c23
matiaslcoulougian <matias.coulougian@ing.austral.edu.ar>
2023-05-28 10:49:53 -0300

    Added docker and db config

--- src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
+++ src/main/kotlin/com/example/snippetmanager/SnippetManagerApplication.kt
@@ -2,10 +2,19 @@ package com.example.snippetmanager
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class SnippetManagerApplication
 
 fun main(args: Array<String>) {
     runApplication<SnippetManagerApplication>(*args)
+}
+
+@RestController
+class AuthorizationController {
+    @GetMapping("/")
+    fun index(@RequestParam("name") name: String) = "Hello, $name!"
 }
---
+++ src/main/kotlin/com/example/snippetmanager/entity/Snippet.kt
@@ -0,0 +1,4 @@
+package com.example.snippetmanager.entity
+
+class Snippet {
+}
